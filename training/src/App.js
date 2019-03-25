e7ef487ca75f72e861ef04da5fed22677da72822
trongbinh15 <trongbinhnguyen15@gmail.com>
2019-03-25 16:48:06 +0700

    Title

--- training/src/App.js
+++ training/src/App.js
@@ -13,8 +13,11 @@ class App extends Component {
   render() {
     return (
       <>
+        <h3>Class + SetState</h3>
         <Counter11 />
+        <h3>Function</h3>
         <Counter12 />
+        <h3>Parent-Child</h3>
         <Counter2 count={15} />
       </>
     );
