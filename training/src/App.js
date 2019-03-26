04272c59bfe6f48299efea560500b91010576b9b
trongbinh15 <trongbinhnguyen15@gmail.com>
2019-03-26 11:39:24 +0700

    Bài 3

--- training/src/App.js
+++ training/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         <Counter12 />
         <h3>Parent-Child</h3>
         <Counter2 count={15} />
-
+        <h3>Get data table</h3>
         <DataTable />
       </>
     );
--- training/src/table.js
+++ training/src/table.js
@@ -1,14 +1,42 @@
 
 import axios from 'axios';
 import React from 'react';
+import ReactTable from 'react-table'
+import 'react-table/react-table.css'
 
 class DataTable extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: null,
+            loaded: false
+        }
+    }
+
     getData = () => {
+        this.setState({ loaded: false });
         axios.get('https://jsonplaceholder.typicode.com/todos')
+            // .then(
+            //     res => {
+            //         res.data.pipe(s => {
+            //             if (s.completed === true) {
+            //                 s.completed = 'Hoàn thành';
+            //             } else {
+            //                 s.completed = 'Chưa hoàn thành';
+            //             }
+            //         });
+            //     }
+            // )
             .then(
                 res => {
                     console.log(res.data);
+                    this.setState({
+                        data: res.data,
+                        loaded: true
+                    });
                 }
+
             )
             .catch(
                 error => {
@@ -16,11 +44,53 @@ class DataTable extends React.Component {
                 }
             );
 
-
     }
+
+
+
     render() {
+
+        const columns = [{
+            Header: 'Id',
+            accessor: 'id'
+        },
+        {
+            Header: 'User Id',
+            accessor: 'userId'
+        },
+        {
+            Header: 'Title',
+            accessor: 'title'
+        },
+        {
+            Header: 'Completed',
+            accessor: 'completed',
+            Cell: props => (props.value == true ? 'Hoàn thành' : 'Chưa hoàn thành')
+        }];
+        const Show = () => {
+            if (this.state.loaded) {
+                return (
+                    <ReactTable
+                        data={this.state.data}
+                        columns={columns}
+                        defaultPageSize={5}
+                        filterable={true}
+                    />);
+            }
+
+            return (
+                <p>Loading...</p>
+            );
+        }
+
+
         return (
-            <button onClick={this.getData} />
+            <>
+                <button onClick={this.getData} >Get data</button>
+                <hr />
+                <Show />
+            </>
+
         );
     }
 
