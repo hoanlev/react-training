fe9a90e924f0a19f1c91a5fb187f0e9a907d7cf0
Vũ Quyền <wakawaka2610@gmail.com>
2019-03-27 08:25:17 +0700

    Routing

--- training/src/App.js
+++ training/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import CounterComponent from './component/counter'
-import TableComponent from './component/table'
+
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo" />
-                    <CounterComponent></CounterComponent>
-                    <TableComponent></TableComponent>
                 </header>
             </div>
         );
--- training/src/index.js
+++ training/src/index.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import CounterComponent from './component/counter';
+import CounterComponent from './component/counter'
+import TableComponent from './component/table'
+import Notfound from './component/not-found'
+import { ListGroup } from 'react-bootstrap'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <ListGroup>
+                <ListGroup.Item>
+                    <NavLink exact activeClassName="active" to="/">Home</NavLink>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <NavLink activeClassName="active" to="/counter">Counter</NavLink>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                    <NavLink activeClassName="active" to="/table">Table</NavLink>
+                </ListGroup.Item>
+            </ListGroup>
+            <Route exact path="/" component={App} />
+            <Route path="/counter" component={CounterComponent} />
+            <Route path="/table" component={TableComponent} />
+            <Route component={Notfound} />
+        </div>
+    </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
