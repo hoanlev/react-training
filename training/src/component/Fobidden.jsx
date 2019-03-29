06164944e769ffe4a12e7bb051bc2098685dda22
Vũ Quyền <wakawaka2610@gmail.com>
2019-03-29 13:00:39 +0700

    Fix roter not redirect.

--- training/src/component/Fobidden.jsx
+++ training/src/component/Fobidden.jsx
@@ -1,14 +1,11 @@
 import React, {Component} from 'react'
 import LoginComponent from './Login';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Switch ,Redirect} from 'react-router-dom'
 const Fobidden = () => 
 <div>
 <h1>Fobidden: do not have enough rights to perform this operation </h1>
 <p>You must log in to view the page</p>
-<Router>
- <Link to='/login' > Login</Link>
- <Route exact path = '/login' component = {LoginComponent}/>
-</Router>
+<a href="/login">aaaa</a>
 </div>
 
 export default Fobidden;
